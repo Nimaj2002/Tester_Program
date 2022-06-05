62529c68a746469705e7c60f4275c0c2c4a33841
Nima Jelodari <nimajelodari2002@gmail.com>
2022-06-06 00:28:39 +0430

    Problem With Not File Found in results Solved

--- main.py
+++ main.py
@@ -19,42 +19,43 @@ def test():
     else:
         for code in students_codes:
             # Deleteting exsisted result
-            remove(f"./results/{code[:-3]}.txt")
-            i = 1
-            passed = 0
-            failed = 0
-            while i <= len(inputs):
-                with open(f"./In/input{i}.txt", "r") as inn:
-                    # Getting input datas
-                    i_data = inn.read()
+            try:
+                remove(f"./results/{code[:-3]}.txt")
+            except FileNotFoundError:
+                pass
+            finally:
+                i = 1
+                passed = 0
+                failed = 0
+                while i <= len(inputs):
+                    with open(f"./In/input{i}.txt", "r") as inn:
+                        # Getting input datas
+                        i_data = inn.read()
 
-                with open(f"./Out/output{i}.txt", "r") as out:
-                    # Getting expexted datas
-                    o_data = out.read()
+                    with open(f"./Out/output{i}.txt", "r") as out:
+                        # Getting expexted datas
+                        o_data = out.read()
 
-                # Getting results
-                r_result = subprocess.run(f"python ./all_codes/{code}", shell=True, capture_output=True, text=True, input=i_data)
-                r_result = r_result.stdout
+                    # Getting results
+                    r_result = subprocess.run(f"python ./all_codes/{code}", shell=True, capture_output=True, text=True, input=i_data)
+                    r_result = r_result.stdout
 
-                print("in",i_data)
-                print(list(o_data))
-                print(list(r_result))
-                # print(f"o_data\n"==r_result)
-                # Comparison between expected results and real results
-                with open(f"./results/{code[:-3]}.txt", "a") as result:
-                    if f"{o_data}\n" == r_result:
-                        result.write(f"test{i} [PASSED]\n")
-                        passed += 1
-                    else:
-                        result.write(f"test{i} [FAILED]\n")
-                        failed += 1
+                    # print(f"o_data\n"==r_result)
+                    # Comparison between expected results and real results
+                    with open(f"./results/{code[:-3]}.txt", "a") as result:
+                        if f"{o_data}\n" == r_result:
+                            result.write(f"test{i} [PASSED]\n")
+                            passed += 1
+                        else:
+                            result.write(f"test{i} [FAILED]\n")
+                            failed += 1
 
-                i += 1
+                    i += 1
 
-            #   Appending percentage an passed and failed
-            with open(f"./results/{code[:-3]}.txt", "a") as file:
-                file.write(
-                    f"Final result :\nPASSED: {passed}\nFAILED: {failed}\nPERCENTAGE: {((passed * 100) / (passed + failed))}")
+                #   Appending percentage an passed and failed
+                with open(f"./results/{code[:-3]}.txt", "a") as file:
+                    file.write(
+                        f"Final result :\nPASSED: {passed}\nFAILED: {failed}\nPERCENTAGE: {((passed * 100) / (passed + failed))}")
 
         return "Finished"
 
